4920b633693678ecbef9b0e5f2ed2ca87e290b5b
1sergeik <1sergeik@gmail.com>
2019-09-25 20:12:08 +0300

    задание 2, Исправления

--- sandbox/src/main/java/ru/stqa/pft/sandbox/distance_2_points.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/distance_2_points.java
@@ -4,7 +4,7 @@ public class distance_2_points {
   public static void main(String[] args) {
     Point p1= new Point(-1,-1);
     Point p2= new Point(2,3);
-    System.out.println("Дистанция между точками p1 ("+p1.x+";"+p1.y+") и p2 ("+p2.x+";"+p2.y+") равна "+p1.distance(p1,p2));
+    System.out.println("Дистанция между точками p1 ("+p1.x+";"+p1.y+") и p2 ("+p2.x+";"+p2.y+") равна "+p1.distance(p2));
   }
 
 }
