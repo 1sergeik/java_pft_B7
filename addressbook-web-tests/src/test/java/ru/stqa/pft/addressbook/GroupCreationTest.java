f47f0f4b6b71d5801b7cb84b9a2a648f41607ba1
1sergeik <1sergeik@gmail.com>
2019-09-28 19:14:22 +0300

    фикстура 2.3

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTest.java
@@ -12,16 +12,17 @@ public class GroupCreationTest {
   public void setUp() throws Exception {
     wd = new FirefoxDriver();
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-  }
-
-  @Test
-  public void testGroupCreation() throws Exception {
     wd.get("http://localhost/addressbook/");
     wd.findElement(By.name("user")).clear();
     wd.findElement(By.name("user")).sendKeys("admin");
     wd.findElement(By.name("pass")).clear();
     wd.findElement(By.name("pass")).sendKeys("secret");
     wd.findElement(By.xpath("//input[@value='Login']")).click();
+
+  }
+
+  @Test
+  public void testGroupCreation() throws Exception {
     wd.findElement(By.linkText("groups")).click();
     wd.findElement(By.name("new")).click();
     wd.findElement(By.name("group_name")).clear();
@@ -32,6 +33,7 @@ public class GroupCreationTest {
     wd.findElement(By.name("group_footer")).sendKeys("test2");
     wd.findElement(By.name("submit")).click();
     wd.findElement(By.linkText("groups")).click();
+    wd.findElement(By.linkText("home")).click();
     wd.findElement(By.linkText("Logout")).click();
   }
 
