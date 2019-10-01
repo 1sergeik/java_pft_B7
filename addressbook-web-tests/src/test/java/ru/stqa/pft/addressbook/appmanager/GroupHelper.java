69a11c5a26eabc401bd1de9fb2f650da25f79a04
1sergeik <1sergeik@gmail.com>
2019-10-01 16:14:29 +0300

    2.9 testGroupModification

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -39,4 +39,13 @@ public class GroupHelper extends HelperBase {
   public WebDriver getWd() {
     return wd;
   }
+
+  public void initGroupModification() {
+    click(By.name("edit"));
+
+  }
+
+  public void submitGroupModification() {
+    click(By.name("update"));
+  }
 }
